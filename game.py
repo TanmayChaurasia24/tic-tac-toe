bccd2f62a846ef36413affc95f4a8e73a4aa2f7b
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2023-09-01 23:40:01 +0530

    Add files via upload

--- game.py
+++ game.py
@@ -8,10 +8,10 @@ def printboard(board):
 def check_winner(board , player):
     for i in range(3):
         if all(board[i][j] == player for j in range(3) or all(board[j][i] == player for j in range(3))):
-            return True;
+            return True
 
-    if all(board[i][i] == player for i in range(3)) or all(board[i][2-i] == player for i in range(3)):
-        return True
+        if all(board[i][i] == player for i in range(3)) or all(board[i][2-i] == player for i in range(3)):
+            return True
         
     return False
 
